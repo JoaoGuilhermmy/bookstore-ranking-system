55b4ce6c8301e3b205f3ecb5289f56124cfdacc9
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2025-12-24 15:48:59 -0300

    Projeto finalizado

--- bst.h
+++ bst.h
@@ -4,9 +4,10 @@
 
 CategoryNode *new_node(int id, char category_name[], char books_file[]);
 CategoryNode *insert_category_node(CategoryNode *root, int id, char category_name[], char books_file[]);
-void in_ordem_traversal(CategoryNode *root);
+void in_ordem_traversal(CategoryNode *root); // Correção: retorno void
 CategoryNode *search_category(CategoryNode *root, char category_name[]);
-void free_tree(CategoryNode *root);
-void clear_scream();
-void pause();
+void free_tree(CategoryNode *root); // Faltava esta declaração
+void pause();                       // Para que outros arquivos possam usar
+void clear_scream();                // Para que outros arquivos possam usar
+
 #endif
--- max_heap.h
+++ max_heap.h
@@ -2,6 +2,7 @@
 #define MAX_HEAP_H
 #include "models.h"
 
+// Correção: Padronizar o nome da struct para MaxHeap (igual ao .c)
 typedef struct MaxHeap
 {
     Book *books;
@@ -9,6 +10,7 @@ typedef struct MaxHeap
     int capacity;
 } MaxHeap;
 
+// Correção: Nomes das funções iguais ao max_heap.c
 MaxHeap *create_heap(int capacity);
 void insert_heap(MaxHeap *heap, Book book);
 void print_heap(MaxHeap *heap);
@@ -16,6 +18,6 @@ void consult_top_one(MaxHeap *heap);
 void heapify_down(MaxHeap *heap, int index);
 void list_top_n(MaxHeap *heap, int n);
 void register_sale(MaxHeap *heap, int isbn, int quantity);
-void free_heap(MaxHeap *heap);
+void free_heap(MaxHeap *heap); // Faltava esta declaração
 
 #endif
